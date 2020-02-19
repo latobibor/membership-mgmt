57029298a4db2d048b0afb71f96d25398f27c4db
Tóth András <toth83andras@yahoo.com>
2020-02-19 17:09:28 +0100

    add mock client for saving changes and mock data for initial values

---
+++ src/clients/mock-fetch.ts
@@ -0,0 +1,29 @@
+import { members } from './mock-data';
+
+type FetchMethod = 'POST' | 'GET' | 'PUT' | 'DELETE';
+
+interface MockFetchOptions {
+  body: object;
+  method: FetchMethod;
+}
+
+interface MockResponse {
+  readonly ok: boolean;
+  readonly status: number;
+  readonly statusText: string;
+  json(): Promise<any>;
+}
+
+class MockResponseClass implements MockResponse {
+  constructor(public readonly ok: boolean, public readonly status: number, public readonly statusText: string) {}
+
+  json(): Promise<any> {
+    return Promise.resolve(members);
+  }
+}
+
+export function fetch(url: string, options?: MockFetchOptions): Promise<MockResponse> {
+  console.log(`API endpoint ${url} was called with ${options}`);
+
+  return Promise.resolve(new MockResponseClass(true, 200, 'Everything was fine'));
+}
---
+++ src/clients/save-access-list.ts
@@ -0,0 +1,38 @@
+import { fetch } from './mock-fetch';
+
+interface MemberAccessChangePayload {
+  members: AccessChange[];
+}
+
+interface AccessChange {
+  person_id: string;
+  role: Role;
+  access_level: AccessLevel;
+}
+
+export enum Role {
+  Customer = 'customer',
+  Manager = 'manager',
+  Employee = 'employee',
+}
+
+export enum AccessLevel {
+  Read = 'read',
+  Write = 'write',
+  Admin = 'admin',
+}
+
+export async function saveAccessChanges(changes: AccessChange): Promise<void> {
+  const saveEndpoint = 'https://wqp-backend.api/access-manager';
+
+  const response = await fetch(saveEndpoint, {
+    method: 'POST',
+    body: {
+      members: changes,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`A ${response.status} error occurred during a call to ${saveEndpoint}: ${response.statusText}`);
+  }
+}
